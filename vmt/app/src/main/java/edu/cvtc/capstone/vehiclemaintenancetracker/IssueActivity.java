f8095d916c21f19a368d977be7e91c2b1a4de861
Pretz333 <wpretzel@hotmail.com>
2021-04-21 19:12:06 -0500

    Add in several tests to make sure the intents out of the Options activity were built and passed correctly

--- vmt/app/src/main/java/edu/cvtc/capstone/vehiclemaintenancetracker/IssueActivity.java
+++ vmt/app/src/main/java/edu/cvtc/capstone/vehiclemaintenancetracker/IssueActivity.java
@@ -2,13 +2,26 @@ package edu.cvtc.capstone.vehiclemaintenancetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class IssueActivity extends AppCompatActivity {
+
+    private int vehicleId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_issue);
+
+        // Grab the intent data
+        Intent receivedIntent = getIntent();
+
+        // Grab the ID with a provided default value
+        vehicleId = receivedIntent.getIntExtra(VehicleOptionActivity.EXTRA_VEHICLE_ID, -1);
+
+        //Toast it to make sure it was passed along, TODO: delete this
+        Toast.makeText(getApplicationContext(), "VehicleID: " + Integer.toString(vehicleId), Toast.LENGTH_LONG).show();
     }
 }
