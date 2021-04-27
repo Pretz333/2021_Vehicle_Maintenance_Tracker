23e41ef840513530a0ff2e6db20bf55b8397bddf
Pretz333 <wpretzel@hotmail.com>
2021-04-27 15:34:15 -0500

    Move the isVINValid method to VerifyUtil, update Unit Tests

--- vmt/app/src/main/java/edu/cvtc/capstone/vehiclemaintenancetracker/VerifyUtil.java
+++ vmt/app/src/main/java/edu/cvtc/capstone/vehiclemaintenancetracker/VerifyUtil.java
@@ -1,5 +1,7 @@
 package edu.cvtc.capstone.vehiclemaintenancetracker;
 
+import java.util.HashMap;
+
 public final class VerifyUtil {
     //Strings containing only capital or lowercase a-z return true
     public static boolean isStringLettersOnly(String str){
@@ -20,4 +22,79 @@ public final class VerifyUtil {
     public static boolean isStringSafe(String str) {
         return str.matches("^[a-zA-Z0-9 -]*$");
     }
+
+    //For VINs
+    public static boolean isVINValid(String VIN, String year){
+        //Vehicles pre-1981 don't follow a set standard that can be easily tested
+        if (Integer.parseInt(year) < 1981) {
+            //If using an API query, we can test vehicles pre-1981
+            //For now, just assume it's good
+            return true;
+        } else if(VIN.length() == 17) { //after 1981, vehicles have a 17 character VIN
+            //Vars needed for the test inside of the loop
+            int runningTotal = 0;
+            int[] multipliers = new int[]{8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2};
+            char[] VINArray = VIN.toCharArray();
+            char c; //More efficient to re-assign it than re-declare it 17 times in the for loop
+
+            for (int i = 0; i < VINArray.length; i++) {
+                c = VINArray[i];
+
+                //Characters Q, I, and O are not used due to their similarity to 1 or 0
+                //VINs are only letters or digits, so we'll check that as well
+                if (!Character.isLetterOrDigit(c) || c == 'Q' || c == 'O' || c == 'I') {
+                    return false;
+                }
+
+                //Check digit test preparation
+                //Convert character to its number, multiply the number by the place
+                //multiplier, and lastly add the result to the runningTotal
+                runningTotal += numberValues.get(c) * multipliers[i];
+            }
+
+            //Check digit test, replace 10 with the character x
+            c = runningTotal % 11 == 10 ? 'x' : Character.forDigit(runningTotal % 11, 10);
+            return VINArray[8] == c;
+        }
+
+        return false;
+    }
+
+    //The HashMap for the VIN check digit test, at the class level so
+    //it isn't destroyed and recreated any more than necessary
+    private static final HashMap<Character, Integer> numberValues = new HashMap<Character, Integer>() {{
+        put('A', 1);
+        put('B', 2);
+        put('C', 3);
+        put('D', 4);
+        put('E', 5);
+        put('F', 6);
+        put('G', 7);
+        put('H', 8);
+        put('J', 1);
+        put('K', 2);
+        put('L', 3);
+        put('M', 4);
+        put('N', 5);
+        put('P', 7);
+        put('R', 9);
+        put('S', 2);
+        put('T', 3);
+        put('U', 4);
+        put('V', 5);
+        put('W', 6);
+        put('X', 7);
+        put('Y', 8);
+        put('Z', 9);
+        put('1', 1);
+        put('2', 2);
+        put('3', 3);
+        put('4', 4);
+        put('5', 5);
+        put('6', 6);
+        put('7', 7);
+        put('8', 8);
+        put('9', 9);
+        put('0', 0);
+    }};
 }
--- vmt/app/src/test/java/edu/cvtc/capstone/vehiclemaintenancetracker/UnitTests.java
+++ vmt/app/src/test/java/edu/cvtc/capstone/vehiclemaintenancetracker/UnitTests.java
@@ -12,26 +12,26 @@ import static org.junit.Assert.*;
 public class UnitTests {
     @Test
     public void VINSimpleCheckDigitTestPasses(){
-        assertTrue(Vehicle.isVINValid("11111111111111111", "1999"));
+        assertTrue(VerifyUtil.isVINValid("11111111111111111", "1999"));
     }
 
     @Test
     public void VINTestsFailIfLessThan17Chars(){
-        assertFalse(Vehicle.isVINValid("B", "1999"));
+        assertFalse(VerifyUtil.isVINValid("B", "1999"));
     }
 
     @Test
     public void VINTestsCatchBadCharacters(){
         //An I, a Q, and an O
-        assertFalse(Vehicle.isVINValid("2C3ID46R4WH170262", "1999"));
-        assertFalse(Vehicle.isVINValid("2C3OD46R4WH170262", "1999"));
-        assertFalse(Vehicle.isVINValid("2C3QD46R4WH170262", "1999"));
+        assertFalse(VerifyUtil.isVINValid("2C3ID46R4WH170262", "1999"));
+        assertFalse(VerifyUtil.isVINValid("2C3OD46R4WH170262", "1999"));
+        assertFalse(VerifyUtil.isVINValid("2C3QD46R4WH170262", "1999"));
     }
 
     @Test
     public void VINTestsWorkWithActualVIN(){
         //A 1998 Chrysler Concorde, apparently
-        assertTrue(Vehicle.isVINValid("2C3HD46R4WH170262", "1998"));
+        assertTrue(VerifyUtil.isVINValid("2C3HD46R4WH170262", "1998"));
     }
 
     @Test
